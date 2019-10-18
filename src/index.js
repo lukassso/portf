322725267b6031b42154ecb616bfbfb506132fac
lukasso <lukasz.koladko@gmail.com>
2019-10-19 00:05:58 +0200

    webpack config

---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+
+import './index.html'
+import './styles/style.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,6 @@
-// Webpack uses this to work with directories
-const path = require('path');
+const env = process.env.NODE_ENV;
 
-// This is main configuration object.
-// Here you write different options and tell Webpack what to do
-module.exports = {
-
-  // Path to your entry point. From this file Webpack will begin his work
-  entry: './src/js/index.js',
-
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript 
-  // minifying and other thing so let's set mode to development
-  mode: 'development',
-
-rules: [
-    {
-      test: /\.js$/,
-      /* ... */
-    },
-    {
-      // Apply rule for .sass, .scss or .css files
-      test: /\.(sa|sc|c)ss$/,
-
-      // Set loaders to transform files.
-      // Loaders are applying from right to left(!)
-      // The first loader will be applied after others
-      use: [
-             {
-               // This loader resolves url() and @imports inside CSS
-               loader: "css-loader",
-             },
-             {
-               // Then we apply postCSS fixes like autoprefixer and minifying
-               loader: "postcss-loader"
-             },
-             {
-               // First we transform SASS to standard CSS
-               loader: "sass-loader",
-               options: {
-                 implementation: require("sass")
-               }
-             }
-           ]
-    }
-]
-
+module.exports = env => {
+  console.log(`🛠️  Ahoy there! Building with ${env} mode using ./webpack/webpack.${env}.js 🛠️  `);
+  return require(`./webpack/webpack.${env}.js`);
 };
