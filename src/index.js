9436eae449db919831e5fa680b0b24c5f8bd3aed
lukasso <lukasz.koladko@gmail.com>
2019-10-20 00:16:07 +0200

    changed server to express and node.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-
-
+AOS.init();
+// jquery.init();
+// import 'aos/dist/aos';
+import AOS from 'aos';
+// import jquery from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'jquery/dist/jquery';
 import './index.html'
 import './styles/style.scss';
+import './js/main2';
+import './js/now-ui-kit';
+import './js/script';
+
